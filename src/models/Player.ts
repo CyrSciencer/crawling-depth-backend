7228d9d9c827dbdfbc112fff80819abaac08838a
cyr rouyrre <you@example.com>
2025-05-30 17:22:15 +0200

    modifs for front-back logic

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,62 +1,75 @@
-import mongoose, { Schema} from 'mongoose';
-import {Cell} from './BaseMap';
+import mongoose, { Schema } from "mongoose";
+import { Cell } from "./BaseMap";
 interface Tool {
-    usage: number;
-    power: number;
+  charge: number;
+  power: number;
+  bonus: String;
 }
 interface consumable {
-    impactStat: string;
-    quantity: number;
+  impactStat: string;
+  quantity: number;
 }
-interface Resource {stone: number;
-    iron?: number;
-    silver?: number;
-    gold?: number;
-    tin?: number;
-    zinc?: number;
-    crystal?: number;}
-
+interface Resource {
+  stone: number;
+  iron?: number;
+  silver?: number;
+  gold?: number;
+  tin?: number;
+  zinc?: number;
+  crystal?: number;
+}
+interface Block {
+  stoneBlock?: number;
+  ironBlock?: number;
+  silverBlock?: number;
+  goldBlock?: number;
+  tinBlock?: number;
+  zincBlock?: number;
+  crystalBlock?: number;
+}
 interface Inventory {
-    resources: Resource;
-    consumables: consumable[];
-    tools: {
-        pickaxe: Tool;
-        axe: Tool;
-    equiped: Tool | Resource | null;   
-}}
+  resources: Resource | null;
+  blocks: Block | null;
+  consumables: consumable[] | null;
+  tools: {
+    pickaxe?: Tool;
+  } | null;
+  equiped: Tool | Block | null;
+}
 interface ModifiedMap {
-    personalID: string; //playerID+mapID
-    map: mongoose.Types.ObjectId;//reference to a base map
-    modifiedCells: Cell[];
-    exitLink: {
-        up: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
-        down: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
-        left: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
-        right: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
-    };
+  personalID: string; //playerID+mapID
+  map: mongoose.Types.ObjectId; //reference to a base map
+  modifiedCells: Cell[];
+  exitLink: {
+    up: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
+    down: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
+    left: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
+    right: mongoose.Types.ObjectId | null; //if not explored, base map, if explored, modified map
+  };
 }
 interface Player {
-    inventory: Inventory;
-    modifiedMaps: ModifiedMap[];//array of modified maps
-    //position on current map.
-    position: {
-        row: number;
-        col: number;
-    };
-    currentMap: string;//ref to the modified map (personnal ID)
-    movementPerTurn: Number;
-    health: Number;
+  inventory: Inventory;
+  modifiedMaps: ModifiedMap[]; //array of modified maps
+  //position on current map.
+  position: {
+    row: number;
+    col: number;
+  };
+  currentMap: string; //ref to the modified map (personnal ID)
+  movementPerTurn: Number;
+  health: Number;
 }
 
-const player = mongoose.model<Player>("Player", new Schema({
+const player = mongoose.model<Player>(
+  "Player",
+  new Schema({
     inventory: { type: Object, required: true },
     modifiedMaps: { type: [Object], required: true },
     position: { type: Object, required: true },
     currentMap: { type: String, required: true },
     movementPerTurn: { type: Number, required: true },
     health: { type: Number, required: true },
-}));
+  })
+);
 
 export default player;
-
-
